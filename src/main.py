737040d679edb6ff3814cf1aa896fea3f2014818
Yuulis <yuulis2005@gmail.com>
2025-02-25 23:07:46 +0900

    ボタン押下時にFilePickerによって選択された画像ファイルのパスを表示する処理を実装。

--- src/main.py
+++ src/main.py
@@ -2,7 +2,32 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.Text(value="Hello, World!"))
+    def on_file_selected(e: ft.FilePickerResultEvent):
+        file_paths = []
+        if e.files:
+            file_paths = [file.path for file in e.files]
+            print(file_paths)
+            upload_file_path_text.value = "\n".join(file_paths)
+        upload_file_path_text.update()
+
+    pick_files_dialog = ft.FilePicker(on_result=on_file_selected)
+    page.overlay.append(pick_files_dialog)
+    upload_file_path_text = ft.Text()
+
+    page.add(
+        ft.Row(
+            [
+                ft.ElevatedButton(
+                    text="Open File",
+                    icon=ft.Icons.UPLOAD_FILE,
+                    on_click=lambda _: pick_files_dialog.pick_files(
+                        allow_multiple=True
+                    ),
+                )
+            ]
+        ),
+        upload_file_path_text,
+    )
 
 
 ft.app(main)
