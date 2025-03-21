dabe56d143bb277a55de3d3e53ead4e265881e26
Yuulis <yuulis2005@gmail.com>
2025-03-21 20:52:45 +0900

    input/outputディレクトリの生成処理の実装, 画面サイズ等の調整

--- src/main.py
+++ src/main.py
@@ -1,12 +1,35 @@
+import os
 import flet as ft
+
+APP_NAME = "ImgConverterDesktop"
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 450
+
+
+# Set the app name and window size
+def app_settings(page, app_name, width, height):
+    page.title = app_name
+    page.window.width = width
+    page.window.height = height
+
+
+# Make directories to store the uploaded and converted files
+def make_dirs():
+    if not os.path.exists("input"):
+        os.makedirs("input")
+
+    if not os.path.exists("output"):
+        os.makedirs("output")
 
 
 def main(page: ft.Page):
+    app_settings(page, APP_NAME, WINDOW_WIDTH, WINDOW_HEIGHT)
+    make_dirs()
+
     def on_file_selected(e: ft.FilePickerResultEvent):
         file_paths = []
         if e.files:
             file_paths = [file.path for file in e.files]
-            print(file_paths)
             upload_file_path_text.value = "\n".join(file_paths)
         upload_file_path_text.update()
 
